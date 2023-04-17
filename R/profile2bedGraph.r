446e2a886999a9f927b1e5cad1e1f5a588958064
Haibo Liu <48487183+haibol2016@users.noreply.github.com>
2023-04-17 15:09:06 -0400

    update documentation

--- R/profile2bedGraph.r
+++ R/profile2bedGraph.r
@@ -1,6 +1,7 @@
 #' A wrapper function to compute MNase accessibility (MACC) scores with one command.
 #'
 #' @param tab a matrix with four columns corresponding to chromosome names, start of the bins, end of the bins and interesting values respectively.
+#' @param path path to a base directory where .bedGraph files will be outputted to
 #' @param path2files a character string giving the name of directory where the new file will be saved.
 #' @param fname a character string giving the name of the file to be created.
 #' @param nn integer indicating the number of decimal places to be used.

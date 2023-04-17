b53cadb204f8b1d882e87a15c9f3904d5ae99a63
Haibo Liu <48487183+haibol2016@users.noreply.github.com>
2023-04-17 11:32:19 -0400

    add import

--- R/read.bam.file.r
+++ R/read.bam.file.r
@@ -3,6 +3,8 @@
 #' @param fname a character showing a path to files to be read. 
 #' @param mc.cores a number of cores for parallel computing.
 #' @param tl an integer for tag shifting.
+#' @importFrom Rsamtools scanBam scanBamParam scanBamFlag
+#' @importFrom parallel mclapply
 #' @keywords bam
 #' @export
 #' @examples
